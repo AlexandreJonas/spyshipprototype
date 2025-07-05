578d02498ee6c985eb1e5aea77d946e022f7b553
Alexandre Jonas <jonassouzafonseca@gmail.com>
2025-07-05 00:25:02 -0300

    Evitando spawn perto do jogador

--- scenes/components/outside_rng_spawner/outside_rng_spawner.gd
+++ scenes/components/outside_rng_spawner/outside_rng_spawner.gd
@@ -3,11 +3,12 @@ extends Node
 @export var mob_path: Path2D
 @export var mob_scene: PackedScene
 @export var is_active: bool = false
-@export var speed : int = 300
+@export var high_speed : int = 300
+@export var low_speed : int = 150
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	print(mob_path.get_children().get(0)) 
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -24,20 +25,25 @@ func _on_mob_timer_timeout() -> void:
 	# Choose a random location on Path2D.
 	var mob_spawn_location : PathFollow2D = mob_path.get_children().get(0)
 	mob_spawn_location.progress_ratio = randf()
+	
+	var player = get_tree().get_first_node_in_group("Player")
+	var distance_player_mob :int = player.position.distance_to(mob_spawn_location.position)
+	
+	if distance_player_mob > 450:
+		# Set the mob's position to the random location.
+		mob.position = mob_spawn_location.position
 
-	# Set the mob's position to the random location.
-	mob.position = mob_spawn_location.position
+		# Set the mob's direction perpendicular to the path direction.
+		var direction = mob_spawn_location.rotation - PI / 2
 
-	# Set the mob's direction perpendicular to the path direction.
-	var direction = mob_spawn_location.rotation - PI / 2
+		# Add some randomness to the direction.
+		direction += randf_range(-PI / 4, PI / 4)
+		mob.rotation = direction
 
-	# Add some randomness to the direction.
-	direction += randf_range(-PI / 4, PI / 4)
-	mob.rotation = direction
+		# Choose the velocity for the mob.
+		var mob_final_speed : int = low_speed if distance_player_mob > 800 else high_speed
+		var velocity = Vector2(mob_final_speed, 0.0)
+		mob.linear_velocity = velocity.rotated(direction)
 
-	# Choose the velocity for the mob.
-	var velocity = Vector2(mob.speed, 0.0)
-	mob.linear_velocity = velocity.rotated(direction)
-
-	# Spawn the mob by adding it to the Main scene.
-	add_child(mob)
+		# Spawn the mob by adding it to the Main scene.
+		add_child(mob)
