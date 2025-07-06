f03774f6f8686e8ef381a3cb2194a61ee449dafb
Alexandre Jonas <jonassouzafonseca@gmail.com>
2025-07-06 18:58:44 -0300

    Add mob spawn em level 1

--- scenes/components/single_mob_spawner/single_mob_spawner.gd
+++ scenes/components/single_mob_spawner/single_mob_spawner.gd
@@ -2,6 +2,7 @@ extends Node
 @export var mob_path: Path2D
 @export var mob_scene: PackedScene
 @export var is_despawn: bool = false
+@export var path_follow_index : int = 0
 @export var spawn_path_point : float = 0.0
 
 # Called when the node enters the scene tree for the first time.
@@ -16,7 +17,7 @@ func _process(delta: float) -> void:
 func spawn() -> void:
 	var mob = mob_scene.instantiate()
 	
-	var mob_spawn_location : PathFollow2D = mob_path.get_children().get(0)
+	var mob_spawn_location : PathFollow2D = mob_path.get_children().get(path_follow_index)
 	mob_spawn_location.progress_ratio = spawn_path_point
 	
 	mob.position =  Vector2(0,0)
