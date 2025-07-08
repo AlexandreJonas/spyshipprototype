68ac1ff8e85a2d9da02fa4d7dedb2c7dfba9521b
Alexandre Jonas <jonassouzafonseca@gmail.com>
2025-07-08 18:45:37 -0300

    collectible despawn function

--- scenes/components/single_collectible_spawner/single_collectible_spawner.gd
+++ scenes/components/single_collectible_spawner/single_collectible_spawner.gd
@@ -4,9 +4,11 @@ extends Node
 @export var collectible_score : int = 10
 @export var time_on_screen : int = 40
 
+var collectible
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	$DespawnTimer.wait_time = time_on_screen
 	spawn()
 
 
@@ -15,7 +17,15 @@ func _process(delta: float) -> void:
 	pass
 
 func spawn() -> void:
-	var collectible = collectible_scene.instantiate()
+	collectible = collectible_scene.instantiate()
 	collectible.collect_score = collectible_score
 	collectible.position = collectible_marker.position
 	add_child(collectible)
+	
+	$DespawnTimer.start()
+	
+func despawn() -> void:
+	collectible.queue_free()
+
+func _on_despawn_timer_timeout() -> void:
+	despawn()
